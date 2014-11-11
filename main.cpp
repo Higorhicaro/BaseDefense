1eef9fcc732b0abe3226bf04beba2d43333b85d4
ikarosouza <ikarolp@hotmail.com>
2014-11-11 15:27:57 -0300

    configurando allegro

--- main.cpp
+++ main.cpp
@@ -1,3 +1,5 @@
+//g++ main.cpp -o main $(pkg-config --cflags --libs allegro-5.0 allegro_image-5.0)
+
 #include <allegro5/allegro.h>
 #include <allegro5/allegro_image.h>
 
@@ -8,8 +10,9 @@ int main()
 	al_init();
 	al_init_image_addon();
 	janela = al_create_display(640, 480);
+	al_clear_to_color(al_map_rgb(255, 255, 255));
 	soldier = al_load_bitmap("soldier.png");
-	//al_draw_bitmap(soldier, 0, 0, 0);
+	al_draw_bitmap(soldier, 10, 10, 0);
 	al_flip_display();
 	al_rest(30.0);
 	return 0;
